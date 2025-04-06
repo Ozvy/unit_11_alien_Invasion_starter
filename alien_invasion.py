543763dc2844b039b35a6371d16f35d53e5795fb
Ozvy <ewhittington@student.cscc.edu>
2025-04-05 21:24:32 -0400

    Created settings file and added an image background which scales to the respective screen size

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,47 @@
 import sys
 import pygame
+from settings import Settings
+class AlienInvasion:
+    """
+    The class that runs basically everything about this game.
+
+
+    Methods: 
+        run_game(): Allows the game to run and function, and sets framerate to 60.
+    """
+    def __init__(self):
+        """
+        Sets the display resolution and name of the window, as well as ensures the program runs. 
+        Runs at the number of fps that is displayed in settings.py. 
+        """
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_w, self.settings.screen_h))
+        pygame.display.set_caption(self.settings.name)
+
+        self.bg = pygame.image.load(self.settings.bg_file)
+        self.bg = pygame.transform.scale(self.bg, (self.settings.screen_w, self.settings.screen_h))
+
+        self.running = True
+        self.clock = pygame.time.Clock()
+    
+    def run_game(self):
+        """
+        Allows the game to run and function, and sets framerate . Also displays the game BG.
+        """
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+            self.screen.blit(self.bg, (0,0))
+            pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
+                    
+
 
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+class Settings:
+    '''
+    Allows the user to change certain things about the game if they wanted to.
+    '''
+    def __init__(self):
+        '''
+        Some aspects of the game that can be changed easily.
+        '''
+        self.name: str = 'Alien Invvasion'
+        self.screen_w = 1200
+        self.screen_h = 800
+        self.FPS = 60
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
